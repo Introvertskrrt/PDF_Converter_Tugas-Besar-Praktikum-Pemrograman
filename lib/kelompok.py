d5a810ae09521122ce60bb191910704e128f90dc
Rizky Ageng Nugroho <102507383+RizkyNugroho666@users.noreply.github.com>
2022-07-02 15:56:32 +0700

    Create kelompok.py

---
+++ lib/kelompok.py
@@ -0,0 +1,8 @@
+def kelompok():
+    print("Anggota Kelompok\n")
+    print("Rizky Ageng Nugroho | 21104045")
+    print("Hashbi Alhadi | 21104054")
+    print("Asep Hudori | 21104064")
+    print("Saddam Rizki Saeful Putranto | 21104056")
+    print("Tri Agung Prayoga | 21104052")
+    print("Ginanjar Pangestu | 21104055")

4f76251cf518acae51f8e28638d363f6c4e472d5
Rizky Ageng Nugroho <102507383+RizkyNugroho666@users.noreply.github.com>
2022-07-02 15:52:46 +0700

    Update docx2pdf.py

--- lib/docx2pdf.py
+++ lib/docx2pdf.py
@@ -1,6 +1,16 @@
 import os
-from time import strftime
-from sys import platform, exc_info
+import shutil
+import tkinter
+import colorama
+import time
+
+from colorama import Fore
+from tkinter import filedialog
+from sys import platform
+
+root = tkinter.Tk()
+root.withdraw()
+colorama.init()
 
 # Counts the number of files in the directory that can be converted
 def n_files(directory):
@@ -16,15 +26,11 @@ def createFolder(directory):
     if not os.path.exists(directory + '/Converted to PDF'):
         os.makedirs(directory + '/Converted to PDF')
 
-    if not os.path.exists(directory + '/Input Files'):
-        os.makedirs(directory + '/Input Files')
-
     else:
         pass
 
 def doc2pdf(doc, ending, newdic): # Convert a file with .doc or .docx format to pdf
-
-    cmd = f"lowriter --convert-to pdf:writer_pdf_Export '{doc}'"
+    cmd = f"lowriter --convert-to pdf:writer_pdf_Export '{doc}'" # CMD Command to convert file to PDF
     os.system(cmd)
     new_file = doc.replace(ending, r".pdf")
 
@@ -44,41 +50,63 @@ def is_tool(name):
 
     return which(name) is not None
 
+# open file dialog using Tkinter GUI
+def selectdocxFile(directory): # Select and Copy file to directory
+    filepath = filedialog.askopenfilename(initialdir="Documents",
+                                          title="Select File",
+                                          filetypes= (("docx files","*.docx"),
+                                          ("all files","*.*")))
+    file = filepath
+
+    file_dir = directory
+    shutil.copy(file, file_dir) # copy selected file to directory
+
+def remove_docx(directory): # Remove docx file in directory after conversion
+    files_in_directory = os.listdir(directory)
+    filtered_files = [file for file in files_in_directory if file.endswith(".docx")]
+
+    for file in filtered_files:
+        path_to_file = os.path.join(directory, file)
+        os.remove(path_to_file)
+
 
 # Main Program !!!
 def doctopdf_convert():
-    print('\nPlease note that this will overwrite any existing PDF files')
-    print('For best results, close Microsoft Word before proceeding')
-    input('Press enter to continue.')
+    print('For best results and quality, close Microsoft Word before proceeding')
+    input(Fore.GREEN+'\nPress enter to continue.'+Fore.WHITE)
 	
     directory = os.getcwd()
 
+    selectdocxFile(directory)
     createFolder(directory)
 	
     if n_files(directory) == 0:
-        print('There are no files to convert')
+        print(Fore.RED+'There are no files to convert'+Fore.WHITE)
         exit()
 		
-	
-    print('Starting conversion... \n')
+    print(Fore.BLUE+'Converting to PDF... \n'+Fore.WHITE)
 
     # Opens each file with Microsoft Word and saves as a PDF
     try:
         if(is_tool('libreoffice') == False):
             from win32com import client
             word = client.DispatchEx('Word.Application')
+
         for file in os.listdir(directory):
             if (file.endswith('.doc') or file.endswith('.docx') or file.endswith('.tmd')):
                 ending = ""
+
                 if file.endswith('.doc'):
                     ending = '.doc'
+
                 if file.endswith('.docx'):
                     ending = '.docx'
+
                 if file.endswith('.tmd'):
                     ending = '.tmd'
+
                 if is_tool('libreoffice'):
-                    #in_file = os.path.abspath(directory + '/' + file)
-                    in_file = os.path.abspath(directory + '\\' + file)
+                    in_file = os.path.abspath(directory + '/' + file)
                     new_file = os.path.abspath(directory + '/Converted to PDF')
                     doc2pdf(in_file, ending, new_file)
 
@@ -90,8 +118,10 @@ def doctopdf_convert():
                     print(new_name)
                     doc.SaveAs(new_file,FileFormat = 17)
                     doc.Close()
-                
     except Exception as e:
         print(e)
 
-    print('\nConversion finished at ' + strftime("%H:%M:%S"))
+    print('\nConversion Finished!')
+    time.sleep(2)
+    remove_docx(directory)
+    os.system('cls')

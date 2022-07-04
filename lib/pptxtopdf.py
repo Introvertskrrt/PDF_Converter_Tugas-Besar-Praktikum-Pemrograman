9d00e8ef7d65e2c1b97b86852e9a5f0e3e8b8236
Rizky Ageng Nugroho <102507383+RizkyNugroho666@users.noreply.github.com>
2022-07-04 18:10:24 +0700

    Update pptxtopdf.py

--- lib/pptxtopdf.py
+++ lib/pptxtopdf.py
@@ -10,7 +10,7 @@ from win32com import client
 
 colorama.init()
 
-def n_files(directory):
+def n_files(directory): # Count .pptx files in the directory
     total = 0
 
     for file in os.listdir(directory):
@@ -18,31 +18,33 @@ def n_files(directory):
             total += 1
     return total
 
-def selectpptxfile(directory):
+# open file dialog using Tkinter GUI
+def selectpptxfile(directory): # Select and Copy file to directory
     filepath = filedialog.askopenfilename(initialdir="Documents",
                                           title="Select File",
-                                          filetypes= (("pptx files","*.pptx"), # File Selection
+                                          filetypes= (("pptx files","*.pptx"), # File Selection (Make sure user only input .pptx file)
                                           ))
     file = filepath
     file_dir = directory
-    shutil.copy(file, file_dir)
+    shutil.copy(file, file_dir) # Copy selected file into program directory
 
-def remove_pptx(directory):
+def remove_pptx(directory): # Remove cache/unnecessary pptx files from directory
     files_in_directory = os.listdir(directory)
-    filtered_files = [file for file in files_in_directory if file.endswith('.pptx')]
+    filtered_files = [file for file in files_in_directory if file.endswith('.pptx')] # Filter format file (make sure program only remove .pptx files)
 
     for file in filtered_files:
         path_to_file = os.path.join(directory,file)
         os.remove(path_to_file)
 
-def createFolder(directory):
+def createFolder(directory): # Create Output Folder
     try:
         if not os.path.exists(directory + 'Output Folder/Pptx to Pdf'):
             os.makedirs(directory + '/Output Folder/Pptx to Pdf')
-        else:
+
+        else: # If Folder already exists, then Skip/Pass
             pass
-    except Exception as e:
-        print(e)
+    except:
+        pass
 
 # Main Program!!!
 def pptxtopdf_convert():
@@ -69,7 +71,7 @@ def pptxtopdf_convert():
                 pdf.SaveAs(output_file, FileFormat = 32) # 32 for pptx
                 pdf.Close()
 
-    except Exception as e:
+    except Exception as e: # If there is an error during conversion, then show the error to user
         print(e)
         os.system('pause')
         os.system('cls')

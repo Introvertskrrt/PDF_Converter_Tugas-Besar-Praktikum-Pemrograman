d20e0e189abea2ebb331abf3b403151092e158c2
Rizky Ageng Nugroho <102507383+RizkyNugroho666@users.noreply.github.com>
2022-07-04 13:44:38 +0700

    Update pdftodocx.py

--- lib/pdftodocx.py
+++ lib/pdftodocx.py
@@ -1,11 +1,13 @@
 import os
+import time
 import shutil
-import colorama
 import subprocess
+import colorama
 
 from colorama import Fore
 from tkinter import filedialog
-from pdf2docx import Converter
+from win32com import client
+from sys import platform
 
 colorama.init()
 
@@ -13,13 +15,13 @@ def n_files(directory):
     total = 0
 
     for file in os.listdir(directory):
-        if file.endswith('.pdf'):
+        if (file.endswith('.doc') or file.endswith('.docx') or file.endswith('.tmd')):
             total += 1
     return total
 
-def create_folder(directory): # Create a folder to put the converted files
+def createFolder(directory):
     try:
-        if not os.path.exists(directory + '/Output Folder/Pdf to Docx'):
+        if not os.path.exists(directory + 'Output Folder/Pdf to Docx'):
             os.makedirs(directory + '/Output Folder/Pdf to Docx')
 
         else:
@@ -27,42 +29,70 @@ def create_folder(directory): # Create a folder to put the converted files
     except:
         pass
 
-# File Selector using tkinter GUI
-def selectpdfFile(directory): # Select and Copy file to directory
+# open file dialog using Tkinter GUI
+def selectpdffile(directory): # Select and Copy file to directory
     filepath = filedialog.askopenfilename(initialdir="Documents",
                                           title="Select File",
-                                          filetypes= (("pdf files","*.pdf"), # File Selection
+                                          filetypes= (("docx files","*.pdf"), # File Selection
                                           ))
     file = filepath
     file_dir = directory
-    shutil.copy(file, file_dir)
+    shutil.copy(file, file_dir) # copy selected file to directory
+
+def pdf2docx(pdf, ending, newdic): 
+    cmd = f"lowriter --convert-to pdf:writer_pdf_Export '{pdf}'" # CMD Command
+    os.system(cmd)
+    new_file = pdf.replace(ending, r".docx")
+
+    if platform =='linux': # For Linux
+        cmdmove = f"mv '{new_file}' '{newdic}'"
+
+    elif platform == 'win32': # For Windows
+        new_file = new_file.replace("/", "\\")
+        cmdmove = f"move '{new_file}' '{newdic}'"
+    
+    os.system(cmdmove)
+    print(new_file)
+
+def remove_pdf(directory): # Remove docx file in directory after conversion
+    files_in_directory = os.listdir(directory)
+    filtered_files = [file for file in files_in_directory if file.endswith(".pdf")]
+
+    for file in filtered_files:
+        path_to_file = os.path.join(directory, file)
+        os.remove(path_to_file)
 
 # Main Program!!!
 def pdftodocx_convert():
     directory = os.getcwd()
-    create_folder(directory)
-    selectpdfFile(directory)
+    createFolder(directory)
+    selectpdffile(directory)
+    n_files(directory)
 
-    if n_files(directory) == 0:
-        print(Fore.RED+"There are no files to convert"+Fore.WHITE)
-        exit()
-
-    print(Fore.BLUE+"Converting file to docx..."+Fore.WHITE)
+    docx = client.Dispatch('Word.Application')
+    docx.visible = 0
+    print("Converting PDF to Docx...")
 
     try:
         for file in os.listdir(directory):
             if file.endswith('.pdf'):
-                pdf_file = file
-                docx_file = pdf_file.replace(".pdf", r".docx")
-                cv = Converter(pdf_file)
-                cv.convert(docx_file, start = 0, end=None)
-                cv.close()
-                shutil.move(docx_file, "Output Folder\\Pdf to Docx\\")
-                os.remove(pdf_file) # Remove docx file after conversion finished
+                ending = ".pdf"
         
-        converted_dir = directory+"\\Output Folder\\Pdf to Docx"
-        subprocess.Popen(f'explorer "{converted_dir}"')
-        os.system('cls')
+                new_name = file.replace(ending,r".docx")
+                input_file = os.path.abspath(directory + '\\' + file)
+                pdf = docx.Documents.Open(input_file)
+                output_file = os.path.abspath(directory + "\\Output Folder\\Pdf to Docx" + '\\' + new_name)
+                print(f"Converted File: {new_name}")
+                pdf.SaveAs(output_file, FileFormat=16)
+                pdf.Close()               
 
     except Exception as e:
         print(e)
+
+    # Program Finished
+    print(Fore.BLUE+"Conversion Success"+Fore.WHITE)
+    time.sleep(2)
+    remove_pdf(directory)
+    converted_dir = directory+"\\Output Folder\\Pdf to Docx"
+    subprocess.Popen(f'explorer "{converted_dir}"')
+    os.system("cls")
